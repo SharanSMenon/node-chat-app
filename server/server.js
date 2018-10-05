07f6f00431142cf2e0869013bb8a5fb53614a725
Sharan Sajiv Menon <sharan@Sharan-MBP.fios-router.home>
2018-10-05 18:22:32 -0400

    Yay

--- server/server.js
+++ server/server.js
@@ -5,6 +5,6 @@ const publicPath = path.join(__dirname, '../public')
 const port = process.env.PORT || 3000;
 var app = express();
 app.use(express.static(publicPath));
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log(`Server is up on ${port}`);
 })
