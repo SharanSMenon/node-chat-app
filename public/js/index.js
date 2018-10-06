ef4e5c9a2a1c5e277db1a6f7de743a7aa9903e41
Sharan Sajiv Menon <sharan@Sharan-MBP.fios-router.home>
2018-10-06 19:42:30 -0400

    Alerts others via console

--- public/js/index.js
+++ public/js/index.js
@@ -1,11 +1,9 @@
 var socket = io();
+
 socket.on('connect', function () {
     console.log('Connected to server')
-    socket.emit('createMessage',{
-        from:'Sharan',
-        text:'Ok'
-    })
 })
+
 socket.on('disconnect', function () {
     console.log('Disconnected from server');
 })
