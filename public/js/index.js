7d54bf0f46f95f9ccf128fb2bdf2546081874ca3
Sharan Sajiv Menon <sharan@Sharan-MBP.fios-router.home>
2018-10-06 10:57:00 -0400

    Lecture 3

---
+++ public/js/index.js
@@ -0,0 +1,15 @@
+var socket = io();
+socket.on('connect', function () {
+    console.log('Connected to server')
+    socket.emit('createMessage',{
+        from:'Sharan',
+        text:'Ok'
+    })
+})
+socket.on('disconnect', function () {
+    console.log('Disconnected from server');
+})
+
+socket.on('newMessage', function(message) {
+    console.log('newMessage', message)
+})
