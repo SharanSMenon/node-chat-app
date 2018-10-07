04b7cc1ed3a95156223b8fd0173cedf4d517f32c
Sharan Sajiv Menon <sharan@Sharan-MBP.fios-router.home>
2018-10-07 14:36:45 -0400

    Added Geolocation

--- server/server.js
+++ server/server.js
@@ -3,7 +3,8 @@ const http = require('http');
 const express = require('express')
 const socketIO = require('socket.io');
 const {
-    generateMessage
+    generateMessage,
+    generateLocationMessage
 } = require('./utils/message');
 
 const publicPath = path.join(__dirname, '../public')
@@ -17,13 +18,15 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New user connected')
     socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'))
-    socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined the chat'))
+    socket.broadcast.emit('newMessage', generateMessage('Admin', 'New User Joined'))
     socket.on('createMessage', (message, callback) => {
         console.log(message)
         io.emit('newMessage', generateMessage(message.from, message.text))
         callback('This is from the server')
     })
-
+    socket.on('createLocationMessage', (coords) => {
+        io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude))
+    })
     socket.on('disconnect', () => {
         console.log("User was disconnected")
     })
