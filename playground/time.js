fa611ca178971f78d1798b28a6b7ff6670831f43
Sharan Sajiv Menon <sharan@Sharan-MBP.fios-router.home>
2018-10-07 15:28:32 -0400

    Formatted timestamps using moment

---
+++ playground/time.js
@@ -0,0 +1,6 @@
+// // Jan 1st 1970 00:00:00 am
+var moment = require('moment');
+var date = moment()
+console.log(moment(1569019101011).fromNow())
+// var date = new Date();
+// console.log(date.getMonth())
