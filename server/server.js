c90463aeca30f5a7ebbcb03c772c07bf8ed2a48d
Sharan Sajiv Menon <sharan@Sharan-MBP.fios-router.home>
2018-10-07 13:18:08 -0400

    Add form using JQuery and show incoming messages

--- server/server.js
+++ server/server.js
@@ -18,8 +18,10 @@ io.on('connection', (socket) => {
     console.log('New user connected')
     socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'))
     socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined the chat'))
-    socket.on('createMessage', (message) => {
+    socket.on('createMessage', (message, callback) => {
+        console.log(message)
         io.emit('newMessage', generateMessage(message.from, message.text))
+        callback('This is from the server')
     })
 
     socket.on('disconnect', () => {
